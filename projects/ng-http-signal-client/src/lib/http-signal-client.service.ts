3554f5c9926c128ad7702eb6d67eb090a34436d7
Dakota Pease <peased22@gmail.com>
2023-05-28 12:06:58 -0800

    trims down http-singal-client library to begin work

--- projects/ng-http-signal-client/src/lib/http-signal-client.service.ts
+++ projects/ng-http-signal-client/src/lib/http-signal-client.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class NgHttpSignalClientService {
+export class HttpSignalClient {
 
   constructor() { }
 }
--- projects/ng-http-signal-client/src/public-api.ts
+++ projects/ng-http-signal-client/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ng-http-signal-client
  */
 
-export * from './lib/ng-http-signal-client.service';
-export * from './lib/ng-http-signal-client.component';
-export * from './lib/ng-http-signal-client.module';
+export * from './lib/http-signal-client.service';
