d62ae7401f5dcf1800505aeee0bc7dbcc1af388e
Dakota Pease <peased22@gmail.com>
2023-05-28 12:08:14 -0800

    Adds get request to http-signal-client library

--- projects/ng-http-signal-client/src/lib/http-signal-client.service.ts
+++ projects/ng-http-signal-client/src/lib/http-signal-client.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Signal, WritableSignal, Injectable, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+
+export interface HttpSignalClientReponse<T> {
+  loading: boolean;
+  error: any | null;
+  data: T | null;
+}
+
+const initialValue = {
+  loading: true,
+  error: null,
+  data: null,
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpSignalClient {
+  private _http: HttpClient = inject(HttpClient);
 
-  constructor() { }
+  get<T>(url: string) {
+    const req$: Observable<HttpSignalClientReponse<T>> = this._http
+      .get<T>(url)
+      .pipe(
+        map((data: T) => ({ loading: false, error: null, data })),
+        catchError((error) => of({ loading: false, error, data: null }))
+      );
+
+    return toSignal(req$, { initialValue });
+  }
 }
